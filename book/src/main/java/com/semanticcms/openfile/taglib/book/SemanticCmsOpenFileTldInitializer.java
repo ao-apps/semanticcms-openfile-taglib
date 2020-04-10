cb55568295b171ace2b6d8ad182358fbf09f722c
AO Industries, Inc <support@aoindustries.com>
2020-04-10 17:58:23 -0500

    Fixed API links.

--- book/src/main/java/com/semanticcms/openfile/taglib/book/SemanticCmsOpenFileTldInitializer.java
+++ book/src/main/java/com/semanticcms/openfile/taglib/book/SemanticCmsOpenFileTldInitializer.java
@@ -34,7 +34,7 @@ public class SemanticCmsOpenFileTldInitializer extends TagReferenceInitializer {
 			"/openfile/taglib",
 			"/semanticcms-openfile.tld",
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
-			Maven.properties.getProperty("javadoc.link.javaee.6"),
+			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			Collections.singletonMap("com.semanticcms.openfile.taglib.", Maven.properties.getProperty("project.url") + "apidocs/")
 		);
 	}
