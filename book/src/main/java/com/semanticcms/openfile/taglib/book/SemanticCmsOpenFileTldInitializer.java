59d77e6441ce11426bb0057096fe358ecc215a07
AO Industries, Inc <support@aoindustries.com>
2020-05-30 00:53:41 -0500

    New option "requireLinks" that will fail when a class does not map to a
    package in "apiLinks".
    Defaults to "false".

--- book/src/main/java/com/semanticcms/openfile/taglib/book/SemanticCmsOpenFileTldInitializer.java
+++ book/src/main/java/com/semanticcms/openfile/taglib/book/SemanticCmsOpenFileTldInitializer.java
@@ -43,6 +43,7 @@ public class SemanticCmsOpenFileTldInitializer extends TagReferenceInitializer {
 				),
 				Path.valueOf("/semanticcms-openfile.tld")
 			),
+			true,
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
