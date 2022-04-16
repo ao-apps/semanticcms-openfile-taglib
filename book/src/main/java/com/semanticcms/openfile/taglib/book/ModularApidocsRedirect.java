a1d3d36d086892a4dff075b4507e6907c0bd4db5
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:41 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/semanticcms/openfile/taglib/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/openfile/taglib/book/ModularApidocsRedirect.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-openfile-taglib - SemanticCMS desktop integration mode for local content creation in a JSP environment.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-openfile-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.openfile.taglib.book;
 
 import com.aoapps.net.URIParametersUtils;
